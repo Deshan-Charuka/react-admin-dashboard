ccb632734fa397e7a7dda5062b6d3f9d43209bbd
Deshan Charuka <d.c.0729439631@gmail.com>
2022-05-07 13:52:23 +0530

    Add Chart component

---
+++ src/Constants/Data.js
@@ -0,0 +1,47 @@
+export const userData = [
+    {
+      name: "Jan",
+      "Active users": 3000,
+    },
+    {
+      name: "Feb",
+      "Active users": 3700,
+    },
+    {
+      name: "Mar",
+      "Active users": 6800,
+    },
+    {
+      name: "Apr",
+      "Active users": 7250,
+    },
+    {
+      name: "May",
+      "Active users": 3450,
+    },
+    {
+      name: "Jun",
+      "Active users": 2000,
+    },
+    {
+      name: "Jul",
+      "Active users": 3060,
+    },
+    {
+      name: "Sep",
+      "Active users": 5000,
+    },
+    {
+      name: "Oct",
+      "Active users": 2700,
+    },
+    {
+      name: "Nov",
+      "Active users": 5060,
+    },
+    {
+      name: "Dec",
+      "Active users": 8000,
+    },
+  ];
+  
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
 import "./Home.css";
 import React from "react";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import Chart from "../../components/chart/Chart";
+import {userData} from "../../Constants/Data"
 
 export default function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart data={userData} title="User Analytics" grid dataKey={"Active users"}/>
     </div>
   );
 }
