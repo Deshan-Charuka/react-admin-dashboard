8064d9161c474ce93e1f7874431b5473b2238243
Deshan Charuka <d.c.0729439631@gmail.com>
2022-05-12 07:51:54 +0530

    Add large widget component

--- src/components/widgetLg/WidgetLg.jsx
+++ src/components/widgetLg/WidgetLg.jsx
@@ -1,10 +1,81 @@
-import React from 'react'
-import "./widgetLg.css"
+import React from "react";
+import "./widgetLg.css";
 
 export default function widgetLg() {
+  const StatusButton = ({ type }) => {
+    return <button className={"widgetLgButton " + type}>{type}</button>;
+  };
   return (
-    <div className='widgetLg'>
-        Large
+    <div className="widgetLg">
+      <h3 className="widgetLgTitle">Latest Transactions</h3>
+      <table className="widgetLgTable">
+        <tr className="widgetLgTr">
+          <th className="widgetLgTh">Customer</th>
+          <th className="widgetLgTh">Date</th>
+          <th className="widgetLgTh">Amount</th>
+          <th className="widgetLgTh">Status</th>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img
+              src="https://images.pexels.com/photos/937483/pexels-photo-937483.jpeg?cs=srgb&dl=pexels-mentatdgt-937483.jpg&fm=jpg"
+              alt=""
+              className="widgetLgImg"
+            />
+            <span className="widgetLgName">Susan Carol</span>
+          </td>
+          <td className="widgetLgDate">2 Jun 2022</td>
+          <td className="widgetLgAmount">$122.30</td>
+          <td className="widgetLgStatus">
+            <StatusButton type={"Approved"} />
+          </td>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img
+              src="https://images.pexels.com/photos/937483/pexels-photo-937483.jpeg?cs=srgb&dl=pexels-mentatdgt-937483.jpg&fm=jpg"
+              alt=""
+              className="widgetLgImg"
+            />
+            <span className="widgetLgName">Susan Carol</span>
+          </td>
+          <td className="widgetLgDate">2 Jun 2022</td>
+          <td className="widgetLgAmount">$122.30</td>
+          <td className="widgetLgStatus">
+            <StatusButton type={"Declined"} />
+          </td>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img
+              src="https://images.pexels.com/photos/937483/pexels-photo-937483.jpeg?cs=srgb&dl=pexels-mentatdgt-937483.jpg&fm=jpg"
+              alt=""
+              className="widgetLgImg"
+            />
+            <span className="widgetLgName">Susan Carol</span>
+          </td>
+          <td className="widgetLgDate">2 Jun 2022</td>
+          <td className="widgetLgAmount">$122.30</td>
+          <td className="widgetLgStatus">
+            <StatusButton type={"Pending"} />
+          </td>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img
+              src="https://images.pexels.com/photos/937483/pexels-photo-937483.jpeg?cs=srgb&dl=pexels-mentatdgt-937483.jpg&fm=jpg"
+              alt=""
+              className="widgetLgImg"
+            />
+            <span className="widgetLgName">Susan Carol</span>
+          </td>
+          <td className="widgetLgDate">2 Jun 2022</td>
+          <td className="widgetLgAmount">$122.30</td>
+          <td className="widgetLgStatus">
+            <StatusButton type={"Approved"} />
+          </td>
+        </tr>                        
+      </table>
     </div>
-  )
+  );
 }
