2f0dfd86f338be9dcf3d25b09b13b6696245d30f
Deshan Charuka <d.c.0729439631@gmail.com>
2022-06-01 13:30:07 +0530

    Add delete functionality

--- src/Constants/Data.js
+++ src/Constants/Data.js
@@ -1,47 +1,130 @@
 export const userData = [
-    {
-      name: "Jan",
-      "Active users": 3000,
-    },
-    {
-      name: "Feb",
-      "Active users": 3700,
-    },
-    {
-      name: "Mar",
-      "Active users": 6800,
-    },
-    {
-      name: "Apr",
-      "Active users": 7250,
-    },
-    {
-      name: "May",
-      "Active users": 3450,
-    },
-    {
-      name: "Jun",
-      "Active users": 2000,
-    },
-    {
-      name: "Jul",
-      "Active users": 3060,
-    },
-    {
-      name: "Sep",
-      "Active users": 5000,
-    },
-    {
-      name: "Oct",
-      "Active users": 2700,
-    },
-    {
-      name: "Nov",
-      "Active users": 5060,
-    },
-    {
-      name: "Dec",
-      "Active users": 8000,
-    },
-  ];
-  
+  {
+    name: "Jan",
+    "Active users": 3000,
+  },
+  {
+    name: "Feb",
+    "Active users": 3700,
+  },
+  {
+    name: "Mar",
+    "Active users": 6800,
+  },
+  {
+    name: "Apr",
+    "Active users": 7250,
+  },
+  {
+    name: "May",
+    "Active users": 3450,
+  },
+  {
+    name: "Jun",
+    "Active users": 2000,
+  },
+  {
+    name: "Jul",
+    "Active users": 3060,
+  },
+  {
+    name: "Sep",
+    "Active users": 5000,
+  },
+  {
+    name: "Oct",
+    "Active users": 2700,
+  },
+  {
+    name: "Nov",
+    "Active users": 5060,
+  },
+  {
+    name: "Dec",
+    "Active users": 8000,
+  },
+];
+
+export const userRows = [
+  {
+    id: 1,
+    userName: "Jon Snow",
+    avatar:
+      "https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    email: "Jsnow@hotmail.com",
+    status: "active",
+    transaction: "$129.00",
+  },
+  {
+    id: 2,
+    userName: "Jon Snow",
+    avatar:
+      "https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    email: "Jsnow@hotmail.com",
+    status: "active",
+    transaction: "$129.00",
+  },
+  {
+    id: 3,
+    userName: "Jon Snow",
+    avatar:
+      "https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    email: "Jsnow@hotmail.com",
+    status: "active",
+    transaction: "$129.00",
+  },
+  {
+    id: 4,
+    userName: "Jon Snow",
+    avatar:
+      "https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    email: "Jsnow@hotmail.com",
+    status: "active",
+    transaction: "$129.00",
+  },
+  {
+    id: 5,
+    userName: "Jon Snow",
+    avatar:
+      "https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    email: "Jsnow@hotmail.com",
+    status: "active",
+    transaction: "$129.00",
+  },
+  {
+    id: 6,
+    userName: "Jon Snow",
+    avatar:
+      "https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    email: "Jsnow@hotmail.com",
+    status: "active",
+    transaction: "$129.00",
+  },
+  {
+    id: 7,
+    userName: "Jon Snow",
+    avatar:
+      "https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    email: "Jsnow@hotmail.com",
+    status: "active",
+    transaction: "$129.00",
+  },
+  {
+    id: 8,
+    userName: "Jon Snow",
+    avatar:
+      "https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    email: "Jsnow@hotmail.com",
+    status: "active",
+    transaction: "$129.00",
+  },
+  {
+    id: 9,
+    userName: "Jon Snow",
+    avatar:
+      "https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    email: "Jsnow@hotmail.com",
+    status: "active",
+    transaction: "$129.00",
+  },
+];
--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -1,137 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "./UserList.css";
-import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
-import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import { userRows } from "../../Constants/Data";
+import { Link } from "react-router-dom";
 
-const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 90 },
-  {
-    field: "user",
-    headerName: "User",
-    width: 200,
-    renderCell: (params) => {
-      return (
-        <div className="userListUser">
-          <img className="userListImg" src={params.row.avatar} alt="" />
-          {params.row.userName}
-        </div>
-      );
-    },
-  },
-  { field: "email", headerName: "Email", width: 200 },
-  {
-    field: "status",
-    headerName: "Status",
-    width: 120,
-  },
-  {
-    field: "transaction",
-    headerName: "Transaction",
-    width: 160,
-  },
-  {
-    field: "action",
-    headerName: "Action",
-    width: 150,
-    renderCell: (params) => {
-      return (
-        <>
-          <button className="userListEdit">Edit</button>
-          <DeleteOutlineIcon className="userListDelete" />
-        </>
-      );
-    }
-  },  
-];
-
-const rows = [
-  {
-    id: 1,
-    userName: "Jon Snow",
-    avatar:
-      "https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    email: "Jsnow@hotmail.com",
-    status: "active",
-    transaction: "$129.00",
-  },
-  {
-    id: 2,
-    userName: "Jon Snow",
-    avatar:
-      "https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    email: "Jsnow@hotmail.com",
-    status: "active",
-    transaction: "$129.00",
-  },
-  {
-    id: 3,
-    userName: "Jon Snow",
-    avatar:
-      "https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    email: "Jsnow@hotmail.com",
-    status: "active",
-    transaction: "$129.00",
-  },
-  {
-    id: 4,
-    userName: "Jon Snow",
-    avatar:
-      "https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    email: "Jsnow@hotmail.com",
-    status: "active",
-    transaction: "$129.00",
-  },
-  {
-    id: 5,
-    userName: "Jon Snow",
-    avatar:
-      "https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    email: "Jsnow@hotmail.com",
-    status: "active",
-    transaction: "$129.00",
-  },
-  {
-    id: 6,
-    userName: "Jon Snow",
-    avatar:
-      "https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    email: "Jsnow@hotmail.com",
-    status: "active",
-    transaction: "$129.00",
-  },
-  {
-    id: 7,
-    userName: "Jon Snow",
-    avatar:
-      "https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    email: "Jsnow@hotmail.com",
-    status: "active",
-    transaction: "$129.00",
-  },
-  {
-    id: 8,
-    userName: "Jon Snow",
-    avatar:
-      "https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    email: "Jsnow@hotmail.com",
-    status: "active",
-    transaction: "$129.00",
-  },
-  {
-    id: 9,
-    userName: "Jon Snow",
-    avatar:
-      "https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    email: "Jsnow@hotmail.com",
-    status: "active",
-    transaction: "$129.00",
-  },
-];
 export default function UserList() {
+  const [data, setData] = useState(userRows);
+
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "user",
+      headerName: "User",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="userListUser">
+            <img className="userListImg" src={params.row.avatar} alt="" />
+            {params.row.userName}
+          </div>
+        );
+      },
+    },
+    { field: "email", headerName: "Email", width: 200 },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 120,
+    },
+    {
+      field: "transaction",
+      headerName: "Transaction",
+      width: 160,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={"/user/" + params.row.id}>
+              <button className="userListEdit">Edit</button>
+            </Link>
+
+            <DeleteOutlineIcon
+              className="userListDelete"
+              onClick={() => {
+                handleDelete(params.row.id);
+              }}
+            />
+          </>
+        );
+      },
+    },
+  ];
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
   return (
     <div className="userList">
       <DataGrid
-        rows={rows}
+        rows={data}
         columns={columns}
         pageSize={8}
         //rowsPerPageOptions={[5,8,7]}
